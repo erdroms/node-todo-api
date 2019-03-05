80a9b95848562abd0bc83fdc2dfefaa63008542e
Erin Droms <erin.droms@rtslabs.com>
2019-03-05 15:58:41 -0500

    add tests for GET /users/me and POST /users

---
+++ server/tests/seed/seed.js
@@ -0,0 +1,59 @@
+const { ObjectID } = require('mongodb');
+const jwt = require('jsonwebtoken');
+
+const { Todo } = require('./../../models/todo');
+const { User } = require('./../../models/user');
+
+const user1Id = new ObjectID();
+const user2Id = new ObjectID();
+
+const users = [{
+  _id: user1Id,
+  email: 'erin@example.com',
+  password: 'user1pass',
+  tokens: [{
+    access: 'auth',
+    token: jwt.sign({ _id: user1Id, access: 'auth' }, 'abc123').toString(), //abc123 is our salt which is defined in user.js (generateAuthToken)
+  }]
+}, {
+  _id: user2Id,
+  email: 'erin2@example.com',
+  password: 'user2pass',
+}]
+
+const todos = [
+  {
+    _id: new ObjectID(),
+    text: 'first test todo'
+  },
+  {
+    _id: new ObjectID(),
+    text: 'second test todo',
+    completed: true,
+    completedAt: 444,
+  }
+];
+
+const populateTodos = (done) => {
+  Todo.remove({}).then(() => {
+    return Todo.insertMany(todos)
+  }).then(() => done());
+};
+
+const populateUsers = (done) => {
+  User.remove({}).then(() => {
+    var userOne = new User(users[0]).save(); //returns a promise
+    var userTwo = new User(users[1]).save(); //returns a promise
+
+    // wait for userOne and userTwo to resolve
+    // must return the promise so we can add .then to the end of this
+    return Promise.all([userOne, userTwo]);
+  }).then(() => done());
+}
+
+module.exports = {
+  todos,
+  populateTodos,
+  users,
+  populateUsers
+}
