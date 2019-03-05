2ab5c33f0532957a85b8d42299d4797ee21cad5c
Erin Droms <erin.droms@rtslabs.com>
2019-03-05 15:18:36 -0500

    hash user password before saving

--- server/models/user.js
+++ server/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 const jwt = require('jsonwebtoken');
 const _ = require('lodash');
+const bcrypt = require('bcryptjs');
 
 var UserSchema = new mongoose.Schema({
   email: {
@@ -73,6 +74,23 @@ UserSchema.statics.findByToken = function (token) {
     'tokens.access': 'auth'
   });
 }
+
+// pre() is a mongoose middleware
+UserSchema.pre('save', function (next) {
+  var user = this;
+
+  // make sure the password has been updated to prevent re-hashing
+  if (user.isModified('password')) {
+    bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(user.password, salt, (err, hash) => {
+        user.password = hash;
+        next();
+      })
+    })
+  } else {
+    next();
+  }
+})
 
 var User = mongoose.model('User', UserSchema);
 
